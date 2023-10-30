8659858d650b01d94456a610dc4a50b1baf8cf61
spearw <spearw@gmail.com>
2023-10-30 16:03:34 +0000

    Update for working virtual serial port

--- tests/setup_virtual_serial_port.sh
+++ tests/setup_virtual_serial_port.sh
@@ -4,4 +4,4 @@ apt-get install socat
 
 echo "done waiting!"
 
-socat -d -d pty,rawer,echo=0,link=/dev/ttyharness0  pty,rawer,echo=0,link=/dev/ttyharness1
+socat -d -d pty,rawer,echo=0,link=/dev/ttyharness0,b38400  pty,rawer,echo=0,link=/dev/ttyharness1,b38400
--- tests/test.sh
+++ tests/test.sh
@@ -3,15 +3,19 @@ PURPLE='\033[1;35m'
 NC='\033[0m' # No Color
 
 echo -e "${PURPLE}Set up integration tests${NC}"
-source daisy.env
-docker-compose build daisy
+sudo docker compose build daisy
 bash setup_virtual_serial_port.sh &
+sleep 5
+# source needs to happen after serial port setup
+source env
 
 echo -e "${PURPLE}Bring up containers${NC}"
-docker-compose up --abort-on-container-exit
+sudo docker compose up --abort-on-container-exit
 
 echo -e "${PURPLE}Run integration tests${NC}"
 pytest test_integration.py
 
 echo -e "${PURPLE}Tear down integration tests${NC}"
 rm test-data/test-output.json
+sleep 10
+killall socat
