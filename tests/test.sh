67a6db4370c404fba1cd3f973f65db18a154834a
spearw <spearw@gmail.com>
2023-10-30 16:46:59 +0000

    Dynamically use correct virtual port

--- tests/test.sh
+++ tests/test.sh
@@ -5,9 +5,13 @@ NC='\033[0m' # No Color
 echo -e "${PURPLE}Set up integration tests${NC}"
 sudo docker compose build daisy
 bash setup_virtual_serial_port.sh &
+
+# Allow ports time to startup
 sleep 5
-# source needs to happen after serial port setup
-source env
+# Symlinks do not work in docker-compose files. Write correct port numbers to be used in .env file
+echo "export DAISY_PORT="$(readlink /dev/ttyharness0) > .env
+echo "export HARNESS_PORT="$(readlink /dev/ttyharness1) >> .env
+source .env
 
 echo -e "${PURPLE}Bring up containers${NC}"
 sudo docker compose up --abort-on-container-exit
